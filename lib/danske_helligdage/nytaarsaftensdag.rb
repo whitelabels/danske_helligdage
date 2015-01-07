2380a0ab5780074a59e2d02f8c9720b0cf1badcd
Elena Tanasoiu <eta@nettopower.dk>
2015-01-07 12:36:15 +0100

    Added New Year's Eve as a holiday

---
+++ lib/danske_helligdage/nytaarsaftensdag.rb
@@ -0,0 +1,20 @@
+require 'danske_helligdage'
+
+module DanskeHelligdage
+  module Nytaarsaftensdag
+
+    def self.included(base)
+      base.class_eval do
+        alias_method :helligdag_without_nytaarsaften, :helligdag
+        alias_method :helligdag, :helligdag_with_nytaarsaften
+      end
+    end
+
+    def helligdag_with_nytaarsaften
+      (month == 12 && day == 31) ? 'Nytårsdag' : helligdag_without_nytaarsaften
+    end
+
+  end
+end
+
+Date.send(:include, DanskeHelligdage::Nytaarsaftensdag)
---
+++ test/test_nytaarsaftensdag.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require 'danske_helligdage'
+
+class NytaarsaftendagTests < Test::Unit::TestCase
+
+  def setup
+    require 'danske_helligdage/nytaarsaftensdag'
+  end
+
+  def teardown
+    # Restore the original Date class
+    Date.send(:include, DanskeHelligdage::Officielle)
+  end
+
+  def test_should_return_the_name_of_the_holiday
+    assert_equal 'Nytårsdag', Date.civil(2014, 12, 31).helligdag
+  end
+
+end
